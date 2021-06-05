b7911c78ce21d2a2d86ba79e7b66a632c2617102
Drakuras <74796092+Drakuras@users.noreply.github.com>
2021-06-05 22:51:04 +0100

    Making the carousel automatically update
    Spent the whole trying to come up with a solution just to end up using a tutorial

--- myscript.js
+++ myscript.js
@@ -1,1 +1,28 @@
-console.log(document.querySelector('.container'))
+
+const images = document.getElementById('slideshow').querySelectorAll('img')
+let i= 0
+
+
+function change_img(){
+    if(i == 0){
+        images[i].style.display = 'inline-block';
+    }
+    else if(i == images.length){
+        images[i-1].style.display = 'none';
+        images[0].style.display = 'inline-block';
+        i = 0;
+    }
+    else{
+        images[i-1].style.display = 'none';
+        images[i].style.display = 'inline-block';
+    }
+    i++;
+
+}
+
+
+let timer = setInterval(() => {
+change_img()}, 2000);
+function stop(){
+    clearInterval(timer)
+}
