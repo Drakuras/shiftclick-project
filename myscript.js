ddd9ad48f3743af3d9807e1eb51a17a30867542f
Drakuras <74796092+Drakuras@users.noreply.github.com>
2021-06-06 22:37:18 +0100

    add next and previous buttons
    -made the next and previous buttons to function however there are still lots bugs with previous button
    -made the carousel loop automatically

--- myscript.js
+++ myscript.js
@@ -1,27 +1,46 @@
 const images = document.getElementById('slideshow').querySelectorAll('img')
-let i= 0
-function changeImg(){
-    if(i == 0){
-        console.log(`image ${i}`)
+let  i = 0;
+//function that moves to the next image onclick
+function next(){
+    if (i==0){
+        console.log(`you're on if`)
+        images[images.length-1].style.display = 'none';
         images[i].style.display = 'inline-block';
     }
-    else if(i == images.length){
- 
-        images[i-1].style.display = 'none';
+    else if (i > 2){
+        console.log(`you're on else if`)
+        i = 0
+        images[images.length-1].style.display = 'none';
         images[0].style.display = 'inline-block';
-        i = 0;
+        
+        
     }
-    else{
-        console.log(`image ${i}`)
-        images[i-1].style.display = 'none';
-        images[i].style.display = 'inline-block';
+    else if (i < images.length){
+        console.log(`you're on else`)
+        images[i-1].style.display='none'
+        images[i].style.display = 'inline-block'
+        
     }
-    i++;
-
+    console.log(i)
+    i++
+    
 }
-
-
-setInterval('changeImg()', 1000);
-
-window.onload = changeImg()
-
+//function that moves backwards
+function previous(){
+    if (i == 0){
+        i = images.length-1
+        console.log(`${images[2]} ${i}`)
+        images[0].style.display = 'none'
+        images[i].style.display = 'inline-block'
+    }
+    else if(i < images.length){
+        images[i].style.display = 'none'
+        images[i-1].style.display = 'inline-block'
+    }
+    i--;
+    
+    
+}
+//using setInterval to run next() every two seconds this allows the carousel to run automatically 
+setInterval('next()', 2000);
+window.onload = next()
