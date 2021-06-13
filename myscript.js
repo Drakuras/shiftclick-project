fa14febd3bacaee2e0b476388e6b59eaee8cc165
Drakuras <74796092+Drakuras@users.noreply.github.com>
2021-06-13 10:53:42 +0100

    Improvement with javascript and HTML/CSS
    made some improvement with JavaScript like making the code smaller and removing bugs like setinterval time not resetting whenever I hit next

--- myscript.js
+++ myscript.js
@@ -2,61 +2,60 @@ const images = document.getElementById('slideshow').getElementsByTagName('img')
 const button = document.getElementById('slideshow').getElementsByTagName('button')
 let i = 0
 let time = 2000
-function imageChanger(){
-    if (i == 0){
-        images[i].style.display = 'inline-block'
-        
-    }
-    else if (i> images.length-1){
-        images[images.length-1].style.display = 'none'
-        images[0].style.display = 'inline-block'
-        i = 0
+//on and off function for the carousel/slideshow
+function stopLooper(){
+    if (button[1].innerHTML == 'ON'){
+        clearInterval(loop);
+        button[1].innerHTML = 'OFF'
     }
     else{
-        images[images.length-1].style.display = 'none'
-        images[i-1].style.display = 'none'
-        images[i].style.display = 'inline-block'
-    }
-    
-    console.log(i)
-    i++
+        loop = setInterval('next()', time);
+        button[1].innerHTML = 'ON'
+    } 
 }
+//next button funtion
 function next(){
-    console.log(time)
     
-    setTimeout(imageChanger)
-
+    if (i > images.length-1){
+        i = 0
+        images[images.length-1].style.display = 'none'
+        images[0].style.display = 'inline-block'
+    }
+    else{
+        if (i-1 > -1){
+            images[i-1].style.display = 'none'
+            images[i].style.display = 'inline-block'  
+        }
+        else{
+            images[i].style.display = 'inline-block'
+        }
+    }
+    console.log(i)
+    clearInterval(loop)
+    loop = setInterval('next()', time);
+    i++  
 }
 function previous(){
-    clearInterval(looper)
-    if (i ==-1){
+    clearInterval(loop);
+    if (i == -1){
         i = images.length-1
         images[0].style.display = 'none'
-        images[i].style.display = 'inline-block'
-       }
-    else if (i < images.length-1){
-        images[i+1].style.display = 'none'
-        images[i].style.display = 'inline-block'
+        images[images.length-1].style.display = 'inline-block'
         
     }
-    else if (i == 0){
-        images[0].style.display = 'none'
-        images[images.length-1].style.display = 'inline-block'
-
+    else{
+        if (i == images.length-1){
+            images[0].style.display = 'none'
+            images[i-1].style.display = 'inline-block'
+        }
+        else{
+            images[i].style.display = 'none'
+            images[i-1].style.display = 'inline-block'
+        }
     }
     console.log(i)
     i--;
 }
-function stopLooper(){
-    if (button[1].innerHTML == 'ON'){
-        clearInterval(looper);
-        button[1].innerHTML = 'OFF'
-    }
-    else{
-        looper
-        button[1].innerHTML = 'ON'
-    }
-    
-    
-}
-let looper = setInterval(imageChanger,time);
+
+images[0].style.display = 'inline-block'
+let loop = setInterval('next()', time);
